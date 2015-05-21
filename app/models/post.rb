71ecc277eae925d45e5cd0745e97b3704debbae6
Zuri Hunter <zurihunter92@gmail.com>
2015-05-21 16:48:42 +0000

    Validations to Post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, length: {minimum: 5}
+  validates :body, presence: true
+  #validates if title and body isn't left blank...go to postscontroller to define new and create method
 end
