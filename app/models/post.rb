0d1765c7e5b9f1c0c9e0dae3b234f06c8ed24527
Zuri Hunter <zurihunter92@gmail.com>
2015-06-03 20:11:16 +0000

    Added Commenting manually

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
   #validates if title and body isn't left blank...go to postscontroller to define new and create method
